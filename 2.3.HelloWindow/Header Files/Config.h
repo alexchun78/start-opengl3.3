dfaca59c4176e91cec69855f1ef12eacf3c38414
alexchun78 <alexchun78@gmail.com>
2019-12-31 15:57:26 +0900

    1st task of learning opengl
    ref. site : https://learnopengl.com, Joey de Vries

---
+++ 2.3.HelloWindow/Header Files/Config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Standard headers */
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+
+/* opengl headers */
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+/* global variables */
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+
+#endif // CONFIG_H
